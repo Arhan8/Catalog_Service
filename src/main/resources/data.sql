1eb6b1e5df7f53693d063837dde0aafdabadb312
veniforjava <eashaan.kurapati@gmail.com>
2019-03-19 01:57:13 +0530

    Catlog_Service_FirstVersion

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into product(id,name,price) values(10,'Laptop',100000);
+insert into product(id,name,price) values(20,'Mobile',50000);
+insert into product(id,name,price) values(30,'Tablet',4000);
+insert into product(id,name,price) values(40,'watch',6000);
